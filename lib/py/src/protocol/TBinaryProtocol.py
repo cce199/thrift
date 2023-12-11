5fa188e42ef8433ebea16ae1d81279c566042fab
cujung <changun.jung@gmail.com>
2023-12-12 00:46:17 +0900

    i32 exception

--- lib/py/src/protocol/TBinaryProtocol.py
+++ lib/py/src/protocol/TBinaryProtocol.py
@@ -116,9 +116,11 @@ class TBinaryProtocol(TProtocolBase):
         self.trans.write(buff)
 
     def writeI32(self, i32):
-        print("TBinaryProtocol-writeI32 : " + str(i32))
-        print(i32)
-        print(type(i32))
+        # print("TBinaryProtocol-writeI32 : " + str(i32))
+        # print(i32)
+        # print(type(i32))
+        if type(i32) <> str:
+            i32 = 0
         buff = pack("!i", i32)
         # print("TBinaryProtocol-writeI32" + str(buff))
         self.trans.write(buff)
